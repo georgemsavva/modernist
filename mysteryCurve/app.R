63fb654ce3298ef56e5db540f6a5fd0ca6f6525a
George Savva <36664190+georgemsavva@users.noreply.github.com>
2020-06-11 11:39:35 +0100

    update

--- mysteryCurve/app.R
+++ mysteryCurve/app.R
@@ -42,13 +42,17 @@ ui <- function(request) {
                                            c("Single value of 'a' (animate with slider)"="single",
                                              "All 'a' superimposed"="simultaneous",
                                              "All 'a' in grid layout"="grid",
+                                             #"Pixel"="pixel",
                                              "Animate over a=(0,1) (make .gif)"="animate")),
                               sliderInput("avalue",label="a",min = 0,max=1, step=0.001,value=0, 
                                           animate=animationOptions(interval=200)),
-                              numericInput("nrowsInGrid","Rows in grid layout",value=3),
+                              fluidRow(
+                                  column(6,numericInput("nrowsInGrid","Rows in grid layout",value=3)),
+                                  column(6,numericInput("oma","Outer Margin",value=1))),
                               radioButtons("drawmode","Draw:", 
                                            c("Segments"="segments",
-                                             "Points"="points")),
+                                             "Points"="points",
+                                             "Pixels (Domain coloring)"="pixels"),),
                               fluidRow(
                                   column(4,textInput("resolution", label="Resolution", "2000")),
                                   column(4,textInput("nSteps", label="length(t)", "1000")),
@@ -133,12 +137,34 @@ server <- function(input, output, session) {
 
     })
 
+    trunc01 <- function(x){
+        pmin(pmax(x,0),1)
+    }
+    
+    mandelbrot <- function(t,max=20){
+        n=rep(0, length(t)) 
+        z=rep(0, length(t))
+        for(i in 1:max){
+            z <- z^2 + t
+            n <- n + ifelse(Mod(z)<10,1,0)
+        }
+        n 
+        }
+    
+    
     output$distPlot <- renderImage({
+        print(Sys.time())
         graphics.off()
+        res=as.numeric(input$resolution)
         set.seed(as.integer(input$seed))
         maxt = eval(parse(text = input$maxt))
         if(input$animatemode=="single") a = input$avalue else a = eval(parse(text=input$nFrames))
+        pl=length(a)
         t = seq(0,maxt,l=as.numeric(input$nSteps))
+        if(input$drawmode=="pixels") {
+            td = as.numeric(input$maxt)*expand.grid( t1=seq(-1, 1, l=res) , t2=seq(-1, 1, l=res))
+            t = 1i*td[,1] + td[,2]
+            }
         d = expand.grid(ai=1:length(a),ti=1:length(t))
         d$t = t[d$ti]
         d$a = a[d$ai]
@@ -150,32 +176,46 @@ server <- function(input, output, session) {
         eval(parse(text = paste0("s <- function(t,a) ", input$sFunction)))
         eval(parse(text = paste0("v <- function(t,a) ", input$vFunction)))
         eval(parse(text = paste0("alpha <- function(t,a) ", input$alphaFunction)))
-        
+        print(Sys.time())
         d$z <- f(d$t,d$a)
-        d$x0 <- Re(d$z)
-        d$y0 <- Im(d$z)
+        d$x0 <- x <- Re(d$z)
+        d$y0 <- y <- Im(d$z)
         d$h <- h(d$t,d$a)
         d$alpha <- alpha(d$t,d$a)
         d$s <- s(d$t,d$a)
         d$v <- v(d$t,d$a)
+        if(input$drawmode=="pixels"){
+            d$hr <- trunc01(Mod(h(d$z,d$a)))
+            d$sr <- trunc01(Mod(s(d$z,d$a)))
+            d$vr <- trunc01(Mod(v(d$z,d$a)))
+            d$rgb <- hsv(d$hr, d$sr, d$vr)
+        }
+        print(Sys.time())
         #d$radius <- radius(d$t,d$a)
         d$lwd <- w(d$t,d$a)
-        l=max(abs(d$z))*as.numeric(input$limitScale)
-        res=as.numeric(input$resolution)
+        l=max(Mod(d$z))*as.numeric(input$limitScale)
+        if(input$drawmode=="pixels") l = as.numeric(input$resolution)
+        lmin=-l
+        if(input$drawmode=="pixels") lmin = 0
         png_path <- file.path(tempdir(), "frame%03d.png")
         png(png_path, type = "cairo-png",width=res,height=res, antialias = "subpixel")
         par(ask = FALSE,mar=c(0,0,0,0), bg=input$bgcol)
         
         if(input$animatemode=="animate"){
-            pl=as.numeric(input$nFrames)
+ 
+            print(paste0("printing ",pl," frames."))
             for (j in 1:pl) {
-                plot(NA,xlim=c(-l,l), ylim=c(-l,l),axes=F, ann=F)
+                plot(NA,xlim=c(lmin,l), ylim=c(lmin,l),axes=F, ann=F)
                 d2 <- d[d$ai==j,]
                 d2$x1 <- c(Re(d2$z)[-1], NA)
                 d2$y1 <- c(Im(d2$z)[-1],NA)
                 if(input$drawmode=="segments") segments(d2$x0,d2$y0, d2$x1,d2$y1, col = hsv(d2$h, d2$s, d2$v, d2$alpha), lwd=d2$lwd)
                 if(input$drawmode=="points") points(d2$x1,d2$y1, col = hsv(d2$h, d2$s, d2$v, d2$alpha), cex=d2$lwd, pch=20)
                 #if(input$drawmode=="circles") symbols(d2$x1,d2$y1, col = hsv(d2$h, d2$s, d2$v, d2$alpha), circles=d2$lwd)
+                if(input$drawmode=="pixels") {
+                    ras <- as.raster(matrix(d2$rgb, nrow=res))
+                    plot(ras, add=TRUE)
+                }
                 
                 print(j)
             }
@@ -184,6 +224,9 @@ server <- function(input, output, session) {
             gif_file <- tempfile(fileext = ".gif")
             gifski(png_files, gif_file,delay = 0.033, width = res,height=res)
             unlink(png_files)
+            rm(d)
+            gc()
+            print(Sys.time())
             return(list(src = gif_file,
                         contentType = 'image/gif'
                         # width = 400,
@@ -194,19 +237,30 @@ server <- function(input, output, session) {
             
         } else {
             pl=1
-            if(input$animatemode!="grid") plot(NA,xlim=c(-l,l), ylim=c(-l,l),axes=F, ann=F)
-            if(input$animatemode=="grid") par(mfrow=c(3,3))
+            if(input$animatemode!="grid") plot(NA,xlim=c(lmin,l), ylim=c(lmin,l),axes=F, ann=F)
+            if(input$animatemode=="grid") par(mfrow=c(3,3), oma=rep(input$oma,4))
             for (j in 1:length(unique(d$a))) {
-                d2 <- d[d$ai==j,]                
+                
+                d2 <- d[d$ai==j,] 
+                #print(d2)
                 d2$x1 <- c(Re(d2$z)[-1], NA)
                 d2$y1 <- c(Im(d2$z)[-1],NA)
-                if(input$animatemode=="grid") plot(NA,xlim=c(-l,l), ylim=c(-l,l),axes=F, ann=F)
+                if(input$animatemode=="grid") plot(NA,xlim=c(lmin,l), ylim=c(lmin,l),axes=F, ann=F)
                 if(input$drawmode=="segments") segments(d2$x0,d2$y0, d2$x1,d2$y1, col = hsv(d2$h, d2$s, d2$v, d2$alpha), lwd=d2$lwd)
                 if(input$drawmode=="points") points(d2$x1,d2$y1, col = hsv(d2$h, d2$s, d2$v, d2$alpha), cex=d2$lwd, pch=20)
+                if(input$drawmode=="pixels") {
+                    ras <- as.raster(matrix(d2$rgb, nrow=res))
+                    #print(ras)
+                    plot(ras, add=TRUE)
+                }
             }
             dev.off()
             png_file <- sprintf(png_path, 1)
-            return(list(src = png_file,
+            rm(d)
+            gc()
+            print(Sys.time())
+            
+                        return(list(src = png_file,
                         contentType = 'image/png',
                         width = "80%",
                         height = "80%"
